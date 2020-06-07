b57dd72de63bec7ca181494308a5a2d5126fa797
Jorge <jorsema2@gmail.com>
2020-06-07 09:18:35 +0200

    Some changes made, not important

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Variables where the settings are stored:
+const sessionLength = document.getElementById('session-duration');
+const breaksNumber = document.getElementById('break-duration');
+const breakLength = document.getElementById('sessions-number');
+
+
+// Object that stores the settings chosen by the user:
+const settings = {
+    sessionLength: 2,
+    breaksNumber: 4,
+    breakLength: 1
+}
+
+// Function that assigns default values:
+
+function defaultValue(key, value, targetElement) {
+    sessionLength.defaultValue = settings.sessionLength;
+    breaksNumber.defaultValue = settings.breaksNumber + 1;
+    breakLength.defaultValue = settings.breakLength;
+}
+
+defaultValue();
+
+// Function and events that assign new values:
+
+// function setValue(key, value, targetElement){
+//     paymentDetails[key] = value;
+// }
+
+// sessionLength.onkeyup = function(event) {
+//     setValue('sessionLength', targetElement.value);
+// }
+
+
+// HTML element that shows the timer
 const shownTimer = document.getElementById('countdown-timer');
 
 // Buttons:
@@ -8,32 +43,25 @@ const resetButton = document.getElementById('reset');
 // Variable called to start and stop the timer:
 let myTimer;
 
-// Variables declared for work duration per period:
-const chosenTimerDuration = 3;
-let countDownTimer = 0;
-let countDownLeft = chosenTimerDuration;
+// // Variables declared for work duration per period:
+let countDownTimer;
+let countDownLeft = settings.sessionLength;
 
-// Variables declard for number of breaks and break duration:
-const breaksNumber = 2;
-const chosenBreakDuration = 2;
-let breaksLeft = breaksNumber;
-let breakDurationLeft = chosenBreakDuration;
+// // Variables declard for number of breaks and break duration:
+let breaksLeft = settings.breaksNumber;
+let breakDurationLeft = settings.breakLength;
 
 // Function that shows counter in HTML:
 
 function timerInHtml(minutes, seconds) {
     if (minutes < 10) {
-        if (seconds < 10) {
-            countDownTimer = '0' + minutes + ':' + '0' + seconds;
-            shownTimer.innerHTML = countDownTimer;
-        } else {
-            countDownTimer = '0' + minutes + ':' + seconds;
-            shownTimer.innerHTML = countDownTimer;
-        }
-    } else {
-        countDownTimer = minutes + ':' + seconds;
-        shownTimer.innerHTML = countDownTimer;
+        minutes = "0" + minutes;
     }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    countDownTimer = minutes + ':' + seconds;
+    shownTimer.innerHTML = countDownTimer;
 }
 
 // Initial timer value;
@@ -59,10 +87,10 @@ function startCount() {
         if (countDownLeft <= 0) {
             shownTimer.innerHTML = "00:00";
             clearInterval(myTimer);
-            countDownLeft = chosenTimerDuration;
+            countDownLeft = settings.sessionLength;
             if (breaksLeft === 0) {
                 startButton.removeAttribute("disabled", "disabled");
-                breaksLeft = breaksNumber;
+                breaksLeft = settings.breaksNumber;
                 return;
             }
             breaks();
@@ -85,7 +113,7 @@ function breaks() {
         if (breakDurationLeft <= 0) {
             shownTimer.innerHTML = "00:00";
             clearInterval(myTimer);
-            breakDurationLeft = chosenBreakDuration;
+            breakDurationLeft = settings.breakLength;
             breaksLeft--;
             startCount();
             return;
@@ -97,8 +125,9 @@ function breaks() {
 }
 
 function resetTimer() {
-    countDownLeft = chosenTimerDuration;
-    breakDurationLeft = chosenBreakDuration;
+    countDownLeft = settings.sessionLength;
+    breakDurationLeft = settings.breakLength;
+    breaksLeft = settings.breaksNumber;    
 
     const seconds = countDownLeft % 60;
     const minutes = Math.floor((countDownLeft - seconds) / 60);
@@ -109,7 +138,11 @@ function resetTimer() {
 // Buttons behaviour:
 startButton.addEventListener('click', function() {
     startButton.setAttribute("disabled", "disabled");
-    startCount();    
+    if (breakDurationLeft < settings.breakLength) {
+        breaks();
+    } else {
+        startCount();
+    }    
 })
 
 stopButton.addEventListener('click', function() {
