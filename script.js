3c6608eb1533c7af6b82aab68362f94485b11104
Jorge <jorsema2@gmail.com>
2020-06-09 15:56:25 +0200

    Now used can and must introduce time in mm:ss format, e.g. (12:18)

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Variables where the settings are stored:
+const form = document.getElementById('form');
 const sessionLength = document.getElementById('session-duration');
 const breakLength = document.getElementById('break-duration');
 const breaksNumber = document.getElementById('sessions-number');
 const applyChanges = document.getElementById('apply-changes');
 
-// Object that stores the settings chosen by the user:
+// Object that stores the settings chosen by the user, at first with the Pomodoro commonly used values:
 const settings = {
     sessionLength: 1500,
     breaksNumber: 1,
@@ -22,35 +23,65 @@ const resetButton = document.getElementById('reset');
 // Variable called to start and stop the timer:
 let myTimer;
 
-// Function that assigns default values:
+// Pomodoro commonly used values being used as input default values:
 
-function defaultValue(key, value, targetElement) {
-    sessionLength.defaultValue = settings.sessionLength;
-    breaksNumber.defaultValue = settings.breaksNumber + 1;
-    breakLength.defaultValue = settings.breakLength;
-}
+sessionLength.defaultValue = '25:00';
+breaksNumber.defaultValue = '2';
+breakLength.defaultValue = '05:00';
 
-defaultValue();
+// Variable to store minutes and seconds converted to seconds:
+let minPlusSec;
 
 // Function and events that assign new values:
 sessionLength.onkeyup = function(event) {
     settings.sessionLength = sessionLength.value;
-    chosenSessionLength = settings.sessionLength;
+    validateField({
+        htmlElement: event.target, 
+        regex: /^([0-5][0-9]):([0-5][0-9])$/, 
+        key: 'sessionLength'
+    });
+    secondsConversion(sessionLength.value);
+    settings.sessionLength = minPlusSec;
     timerInHtml(settings.sessionLength);
 }
 
 breakLength.onkeyup = function(event) {
     settings.breakLength = breakLength.value;
+    validateField({
+        htmlElement: event.target, 
+        regex: /^([0-5][0-9]):([0-5][0-9])$/, 
+        key: 'breakLength'
+    });
+    secondsConversion(settings.breakLength);
+    settings.breakLength = minPlusSec;
 }
 
 breaksNumber.onkeyup = function(event) {
     settings.breaksNumber = breaksNumber.value - 1;
+    validateField({
+        htmlElement: event.target, 
+        regex: /^([0-9])+$/, 
+        key: 'breaksNumber'
+    });
 }
 
 function setValues() {
     chosenSessionLength = settings.sessionLength;
     chosenBreaksNumber = settings.breaksNumber;
     chosenBreaksLength = settings.breakLength;
+}
+
+// Function that converts to seconds:
+
+function secondsConversion(inputValue) {
+    let min = inputValue.substring(0,2);
+    let sec = inputValue.substring(3);
+
+    // Convert string to number:
+    min = parseInt(min);
+    sec = parseInt(sec);
+
+    minPlusSec = min * 60 + sec;
 }
 
 // function setValue(key, value, targetElement){
@@ -141,7 +172,7 @@ function breaks() {
 function resetTimer() {
     settings.sessionLength = chosenSessionLength;
     settings.breakLength = chosenBreaksLength;
-    settings.breaksNumber = chosenBreaksNumber;    
+    settings.breaksNumber = chosenBreaksNumber; 
 
     timerInHtml(settings.sessionLength);
 }
@@ -149,6 +180,7 @@ function resetTimer() {
 // Buttons behaviour:
 startButton.addEventListener('click', function() {
     startButton.setAttribute('disabled', 'disabled');
+    document.getElementById('apply-changes').disabled = true;
     if (chosenBreaksLength < settings.breakLength) {
         breaks();
     } else {
@@ -164,9 +196,59 @@ stopButton.addEventListener('click', function() {
 resetButton.addEventListener('click', function() {
     clearInterval(myTimer);
     resetTimer();
-    startButton.removeAttribute('disabled', 'disabled');    
+    startButton.removeAttribute('disabled', 'disabled');
+    applyChanges.disabled = false;
 })
 
 applyChanges.addEventListener('click', function() {
     setValues();
 })
+
+// Object for later validation:
+
+const isDataValid = {
+    sessionLength: true,
+    breaksNumber: true,
+    breakLength: true   
+}
+
+// Function for validation:
+
+function validateField(config){
+    const validationResult = config.regex.test(settings[config.key]);
+    console.log(validationResult);
+    if (settings[config.key] == '') {
+        config.htmlElement.classList.remove('error');
+        return true;
+    } else if(validationResult === false) {
+        config.htmlElement.classList.add('error');
+        isDataValid[config.key] = false;
+        return false;
+    } else {
+        config.htmlElement.classList.remove('error');
+        isDataValid[config.key] = true;
+    }
+}
+
+// If all data is valid, enable apply-changes button:
+applyChanges.disabled = true;
+
+form.addEventListener('focusout', function() {
+    if (isDataValid.sessionLength && isDataValid.breaksNumber && isDataValid.breakLength) {
+        applyChanges.style.backgroundColor = 'green';
+        applyChanges.style.border = 'green';
+        applyChanges.disabled = false;
+    } else {
+        applyChanges.style.backgroundColor = '';
+        applyChanges.style.border = '';
+        applyChanges.disabled = true;
+    }
+})
+
+// Disable start button when data is modified but user hasn't clicked "apply-changes" yet.
+
+// If the count isn't running, the stop button doesn't appear.
+
+// If the count is running, the start button and the reset button don't appear AND the "apply-changes" button is disabled.
+
+// Disable "apply changes" while the counter is running.
